2f7f847a5cc71072ac7b80aa1828810504fcfd20
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-06-26 11:44:30 +0000

    2

---
+++ vars+data_types.py
@@ -0,0 +1,85 @@
+# variables - a refernce - the name of a variable 
+# - a section of memory
+# - memory reserved and cant be altered unless called by name 
+
+# naming convention - names should be descriptive, start with a lowercase, nover a number.
+# see pep-8 style guide. 
+
+#age = 10
+#person_one_age = 55 # snake_case
+#personOneAge =  55 # camel case
+
+#Var = 55 # avoid capital lettes
+#1var = 55 # dont start with a number
+#VAR = 55 # avoid all caps
+#print = # avoid python keywords
+
+# built in functions
+# print() - print to standard output.
+# type() - returns data type of variable.
+# input() - user input through stardard input defualts to a string.
+
+#x = 10
+#y = "hello"
+#z = True
+#v = 1.03
+
+#print(type(v))
+
+# data types:
+# string - "str" can bve a word, paragrapgh. used it display info. 
+# integer - "int" a whole number
+# float - decimal number
+# boolean - "bool" - True or False  
+
+#name = input("what is your name?: ")
+
+
+#print("your name is" + " " + name)
+#print("your name is", name)
+#print(f"your name is {name}")
+#print(type(name))
+
+#age = input("enter your age: ")
+#age1 = int(input("enter age: "))
+
+#print(type(age))
+#print(type(age1))
+
+#name = "rex"
+#age =  3
+#bark = True
+#meow = False
+
+#print("statement:", name, "barks:", bark)
+
+# Escape characters:
+# \\ backlash, \" double quotes, \n - new line \t - tab, \u uniocode \U extend unicode.
+# 
+#print("my name is \"chris\"") 
+
+#print("person 1: \tHey how are you?\nPerson 2: \tGood thanks! \U0001F604")
+
+# String methods
+
+#print("hEllO WorLD".lower())
+#print("hello world".upper())
+#print("hello world".replace("world", "class"))
+#print("hello world".count("o"))
+#print("hello world".split(","))
+
+#BODMAS 
+
+#brackets
+#order by
+#division
+#multiplication
+#addition
+#subtraction
+
+# to the power of ** 2**10
+#/ true division 10/3 = 3.3336
+#// floor division 10//3 = 3
+#% modulo 10%3 = 1
+
+
