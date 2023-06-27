0ed1769f66c57486c81c4473615c5713a9f807d5
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-06-27 12:50:22 +0000

    2

--- extra_if_task.py
+++ extra_if_task.py
@@ -6,3 +6,21 @@
 #If the largest number is odd and not a multiple of 3, print "Odd Number".
 
 #DONT USE max(a,b,c): PROHIBITED!!
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
+
+largest = a
+
+if b > largest:
+    largest = b
+if c > largest:
+    largest = c
+
+if largest % 2 == 0:
+    print("Even Number")
+elif largest % 2 != 0 and largest % 3 == 0:
+    print("Odd Number and a Multiple of 3")
+else:
+    print("Odd Number")
