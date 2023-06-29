a35db736b9dd4cc79c187b217ddc37245eb8fd0f
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-06-29 13:21:35 +0000

    2

---
+++ rps.py
@@ -0,0 +1,29 @@
+# Make a rock paper scissors app that I can play against the computer. 
+# The program should keep track of the number of rounds played, the number of rounds
+# won by the user, and the number of rounds won by the computer. 
+# After each round, the program should display the results 
+# and ask the user if they want to play another round. 
+# You should use functions, loops, conditionals, random number generation to achieve this.
+
+def function(): # simulates 1 round being played
+    random for computer choice
+    user choice (stored in a list)
+
+    if same:
+        return tie
+    
+    if user == rock
+        if compuet ==
+        return User
+    
+
+rounds = 
+user wins = 
+computer wins =
+
+while:
+    result= fucntion()
+    if == user 
+    +1 user wins
+    print rounds played
+    prinbt user wins 
