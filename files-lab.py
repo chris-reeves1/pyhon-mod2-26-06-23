1b4dfcbb13ed698b900a5817fb73abe0cfa3a643
chris-reeves1 <dr_gonzo19@hotmail.com>
2023-07-03 11:00:30 +0000

    2

---
+++ files-lab.py
@@ -0,0 +1,20 @@
+import csv
+
+companies = []
+sales = []
+
+with open..... as ...:
+    read with csv.reader(alias)
+    next() - can skip over first line
+    for loop to iterate through each line:
+        append to companies
+        append to sales
+
+monthly sum = [list comprehension - use zip(*sales) - to unpack data] 
+
+yearly_sum = {}
+for loop to add companies as keys and sum of sales to values
+    
+figure out a nice way to print out the results
+
+    
